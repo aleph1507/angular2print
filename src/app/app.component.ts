69e9a9c153b13f23452fe9ed2f2428bf469d109b
aleph1507 <xrristo@gmail.com>
2018-04-27 17:31:44 +0200

    print-angular-5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  printInput1 = "Print Input 1";
+  printInput2 = "Print Input 2";
+
+  print(): void {
+    let printContents, popupWin;
+    // printContents = document.getElementById('print-this').innerHTML;
+    printContents = `
+        <h1>Print Document</h1>
+        First Name: ${this.printInput1}<br><hr><br>Last Name: ${this.printInput2}`;
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    popupWin.document.open();
+    popupWin.document.write(`
+      <html>
+        <head>
+          <title>PRINT</title>
+          <link rel="stylesheet" href="app.component.css">
+          <style>
+
+          </style>
+        </head>
+    <body onload="window.print();window.close()">${printContents}</body>
+      </html>`
+    );
+
+    console.log(printContents);
+    popupWin.document.close();
+  }
 }
